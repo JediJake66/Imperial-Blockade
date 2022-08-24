ea0e7b52f7078bd6179c583aafd8e0edd8658654
JediJake66 <jacobstout59@gmail.com>
2022-08-24 14:31:52 -0400

    initial project build

---
+++ bench-app-server-side/src/main/java/com/darksidedev/benchappserverside/BenchAppServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.darksidedev.benchappserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchAppServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchAppServerSideApplication.class, args);
+	}
+
+}
---
+++ bench-app-server-side/src/test/java/com/darksidedev/benchappserverside/BenchAppServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darksidedev.benchappserverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenchAppServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
